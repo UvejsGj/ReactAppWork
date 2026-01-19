2ad8a07afbb5e4ce3f7b3860af7be5c13cfcd905
Uvejs Gjelaj <61552503+UvejsGj@users.noreply.github.com>
2026-01-19 13:32:39 +0100

    data
    data/posts, delete button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+// import Posts from "./Components/Posts";
+import { useMutation, useQueryClient, useQuery } from "@tanstack/react-query";
 
 function App() {
+  const queryClient = useQueryClient();
+
+  const fetchPosts = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    if (!res.ok) throw new Error("Failed to fetch posts");
+    return res.json();
+  };
+
+  const {
+    data: posts,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+  });
+
+  
+
+  const mutation = useMutation({
+    mutationFn: (deletePostId) => {
+      return fetch(
+        `https://jsonplaceholder.typicode.com/posts/${deletePostId}`,
+        {
+          method: "DELETE",
+        },
+      );
+    },
+    onSuccess: (_, deleteId) => {
+      // queryClient.invalidateQueries({ queryKey: ["posts"] });
+      queryClient.setQueryData(["posts"], (oldPosts) => {
+        return oldPosts.filter((post) => post.id !== deleteId);
+      });
+    },
+  });
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  } else if (isError) {
+    return <h2>Error fetching posts</h2>;
+  }
+
+  // if (isError) {
+  //   return <h2>Error</h2>;
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      {posts.map((post) => (
+        <div
+          key={post.id}
+          style={{ border: "1px solid gray", margin: "10px", padding: "10px" }}
         >
-          Learn React
-        </a>
-      </header>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+          <button onClick={() => mutation.mutate(post.id)}>Delete</button>
+          
+        </div>
+
+      ))}
     </div>
   );
 }
